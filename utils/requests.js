4dc147470d2550388d84405bdce171c5d7eebf3c
Glenn Palic <noexgp@gmail.com>
2024-04-17 23:22:10 +0800

    update request.js

--- utils/requests.js
+++ utils/requests.js
@@ -1,9 +1,9 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
-//Fetch all Properties
+// Fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {
   try {
-    //handle the case where the domain is null
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return []
     }
@@ -12,9 +12,11 @@ async function fetchProperties({ showFeatured = false } = {}) {
       `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
       { cache: 'no-store' }
     )
+
     if (!res.ok) {
-      throw new Error('Failed to fetched data')
+      throw new Error('Failed to fetch data')
     }
+
     return res.json()
   } catch (error) {
     console.log(error)
@@ -22,18 +24,20 @@ async function fetchProperties({ showFeatured = false } = {}) {
   }
 }
 
-//Fetch single property
+// Fetch single property
 async function fetchProperty(id) {
   try {
-    //handle the case where the domain is null
+    // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return null
     }
 
     const res = await fetch(`${apiDomain}/properties/${id}`)
+
     if (!res.ok) {
-      throw new Error('Failed to fetched data')
+      throw new Error('Failed to fetch data')
     }
+
     return res.json()
   } catch (error) {
     console.log(error)
