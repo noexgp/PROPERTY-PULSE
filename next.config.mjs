f575260941f4ecdc7412a0de9683eb8046864687
Glenn Palic <noexgp@gmail.com>
2024-04-17 21:17:42 +0800

    corrected next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // NOTE: Use VERCEL_URL to dynamically set NEXTAUTH_URL for Vercel deployments,
-    // falling back to localhost for local development.
     NEXTAUTH_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000',
+    NEXT_PUBLIC_API_DOMAIN: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api`
+      : 'http://localhost:3000/api',
+    NEXT_PUBLIC_DOMAIN: process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : 'http://localhost:3000',
   },
